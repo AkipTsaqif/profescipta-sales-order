dc5d60c9922fadb2635d3c09ef433291c3c353ac
AkipTsaqif <akiptsaqif@gmail.com>
2024-11-12 16:17:04 +0700

    view untuk item detail sudah selesai, on progress crud

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,7 @@ namespace Profescipta_Sales_Order.Controllers
             return View();
         }
 
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
